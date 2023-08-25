914a4c3f07cef0463d74408e9fadd00ec3fc79ed
Amzat19 <kunmiamzat1@gmail.com>
2023-08-25 13:05:23 +0100

    Created Search Form Section

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,15 +1,20 @@
 const SearchForm = () => {
     return (
-        <section>
-            <label>
-                <input />
+        <section className="p-8">
+            <h2 className="mb-4 font-semibold text-lg">Search for Rockets</h2>
+            <div className="flex flex-wrap gap-4">
+           <label>
+                <input type="text" className="border border-black h-12 w-60 rounded-md"/>
             </label>
             <label>
-                <input />
+                <input type="text" className="border border-black h-12 w-60 rounded-md"/>
             </label>
             <label>
-                <input />
+                <input type='text' className="border border-black h-12 w-60 rounded-md"/>
             </label>
+            <button className="h-12 text-white text-lg font-semibold bg-slate-600 w-48 rounded-md">Search</button>
+
+            </div>
         </section>
     )
 }
